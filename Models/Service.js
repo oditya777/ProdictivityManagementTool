53dfad7c0cddc439388cc146d762cca8c5052865
Tanmay Arora <97460992+tanmay-0017@users.noreply.github.com>
2024-07-04 14:05:35 +0530

    Revert "add some files"

--- Models/Service.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const serviceSchema = new mongoose.Schema({
-
-    serviceNum: { type: Number, required: true },
-    serviceType: { type: String, required: true }
-});
-
-export const Service = mongoose.model('Service', serviceSchema);
